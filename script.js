04e44c7c505afe89a21af179d1aa6830dcae3451
RASHID KHAN <156903496+Rashidkhan1234567@users.noreply.github.com>
2024-06-22 07:33:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-econst container = document.getElementById("container");
+const container = document.getElementById("container");
 const registerBtn = document.getElementById("register");
 const loginBtn = document.getElementById("login");
 
